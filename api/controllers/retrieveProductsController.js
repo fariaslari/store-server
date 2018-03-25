51220a3e3e04708c2dc802226084fc1103b10ccf
Larissa Farias <farias_lari@hotmail.com>
2018-03-25 20:34:55 -0300

    Inclusão Header em response para permitir requisições GET

--- api/controllers/retrieveProductsController.js
+++ api/controllers/retrieveProductsController.js
@@ -5,5 +5,7 @@ const products = require('../json/products.json');
 exports.retrieve = function(req, res) {
   let objJson = products;
 
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods', 'GET');
   res.json(objJson);
 };
